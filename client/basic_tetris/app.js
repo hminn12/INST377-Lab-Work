e528ec9e761c0c5ffaaf43f6dbbe7c42b7e99f81
hminn12 <dmcreary48@gmail.com>
2022-02-18 22:09:12 -0500

    app.js

--- client/basic_tetris/app.js
+++ client/basic_tetris/app.js
@@ -107,4 +107,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     draw();
   }
+
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  }
 });
