b09d0fc7f5c69b9f23f8d8034c415e46e9a28e24
hminn12 <dmcreary48@gmail.com>
2022-02-18 22:43:52 -0500

    app.js

--- client/basic_tetris/app.js
+++ client/basic_tetris/app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   let nextRandom = 0;
   let timerId;
+  let score = 0;
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -92,6 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore;
     }
   }
 
@@ -155,4 +157,22 @@ document.addEventListener('DOMContentLoaded', () => {
       displayShape();
     }
   });
+
+  function addScore() {
+    for (let i = 0; i < 199; i +=width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9];
+
+      if(row.every(index => squares[index].classList.contains('taken'))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach(index => {
+          squares[index].classList.remove('taken');
+          squares[index].classList.remove('tetromino');
+          squares[index].style.backgroundColor = '';
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach(cell => grid.appendChild(cell));
+      }
+    }
 });
