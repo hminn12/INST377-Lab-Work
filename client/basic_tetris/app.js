ec76e146bc0cebe5f25a03f8443631e7c28fac02
hminn12 <dmcreary48@gmail.com>
2022-02-18 22:06:41 -0500

    app.js

--- client/basic_tetris/app.js
+++ client/basic_tetris/app.js
@@ -59,7 +59,18 @@ document.addEventListener('DOMContentLoaded', () => {
   timerId = setInterval(moveDown, 1000);
 
   function control(e) {
-    if (e.keyCode === 37) moveLeft();
+    if (e.keyCode === 37) {
+      moveLeft();
+    }
+    else if (e.keyCode === 38) {
+      rotate();
+    }
+    else if (e.keyCode === 39) {
+      moveRight();
+    }
+    else if (e.keyCode === 40) {
+      moveDown();
+    }
   }
   document.addEventListener('keyup', control)
   function moveDown() {
@@ -84,6 +95,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
     if (!isAtLeftEdge) currentPosition -= 1;
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) currentPosition += 1;
+    draw();
+  }
+
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
+    if (!isAtRightEdge) currentPosition += 1;
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
     draw();
   }
 });
